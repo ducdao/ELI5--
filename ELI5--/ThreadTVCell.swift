ebfba658b8fcbd89e3a62febe84ef72995e9cf7a
ducdao <ducdao11@gmail.com>
2017-07-19 19:39:43 -0700

    Added MainTVController and DetailViewController. Created cells.

---
+++ ELI5--/ThreadTVCell.swift
@@ -0,0 +1,15 @@
+//
+//  ThreadTVCell.swift
+//  ELI5--
+//
+//  Created by CheckoutUser on 7/19/17.
+//  Copyright © 2017 CheckoutUser. All rights reserved.
+//
+
+import UIKit
+
+class ThreadTVCell: UITableViewCell {
+   @IBOutlet weak var threadTitleLabel: UILabel!
+   @IBOutlet weak var categoryLabel: UILabel!
+   @IBOutlet weak var hoursSincePostLabel: UILabel!
+}
